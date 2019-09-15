c51f32820ace9d1204effee1264f85a0dc7cf9ce
Vivian <vn.gomez@uniandes.edu.co>
2019-09-15 17:09:35 -0500

    week 5

--- src/components/VSLearnProcess.js
+++ src/components/VSLearnProcess.js
@@ -68,12 +68,27 @@ class VSLearnProcess extends React.Component {
           </div>
           <div className="tz-gallery">
           <div className="caption">
-            <h5><b>Week 4: "Movable helicopter around an object"</b></h5>
+            <h5><b>Week 4: Movable helicopter around an object</b></h5>
             <h6>WebGL Scene graph and rotation </h6>
             </div>
             <Codepen 
             title="Semana 4: Move around an object"
             hash="aboqGGm" 
+            user="viviangomez" 
+            height="500px"
+            defaultTab="js,result"
+            loader={() => <div>Loading...</div>}
+            preview="false"
+            />
+          </div>
+          <div className="tz-gallery">
+          <div className="caption">
+            <h5><b>Week 5: Camera controls</b></h5>
+            <h6>Change the view matrix of the camera </h6>
+            </div>
+            <Codepen 
+            title="Week 5: Camera controls"
+            hash="ExYpQaB" 
             user="viviangomez" 
             height="500px"
             defaultTab="js,result"
